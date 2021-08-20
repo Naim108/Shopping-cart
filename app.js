4e9f43eec54e7b08988070d8f8a79565189648b0
Md Naim Hossain <53082844+Naim108@users.noreply.github.com>
2021-08-20 21:24:16 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const div1=document.getElementById('first'); const div2=document.getElementById('second');
+const div3=document.getElementById('third'); const img=document.getElementById('art');
+div1.addElementListener('click', function(){ img.src='images/1.jpg' }); div2.addElementListener('click',function(){
+img.src='images/2.jpg' }); div2.addElementListener('click',function(){ img.src='images/3.jpg'});
+
+<!--
+Visual Code Mobile
+Developed By Manish Nirmal
+App Available on Play Store :-
+https://play.google.com/store/apps/details?id=lk.visual.code.mobile
+YouTube :-
+https://youtube.com/ManishNirmal
+-->
